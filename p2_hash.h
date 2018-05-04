4dab49dc85adcfaf33a920fc90ec361251ce9c1f
Unknown <jdepoyst@uwyo.edu>
2018-05-03 22:27:52 -0600

    Updated Source Code Files

--- p2_hash.h
+++ p2_hash.h
@@ -7,11 +7,11 @@
 
 /*	
 	Contains parts 4 - 5 of project 2. This includes:
-		-Text to int hash function (~line ??????????????????????????????????????????????
+		-Text to int hash function (~line 27)
 		(slashHash is what I would wish to be graded for part 4. 
-		-Another hash for the bloom filter(~line ?????????????????????????????????????????
-		-Bloom filter function (~line ????????????????????????????????????????????
-*/
+		-Another hash for the bloom filter(~line 50)
+		-Bloom filter function (~line 73)
+*/		
 
 #include <iostream>
 #include <vector>
@@ -60,10 +60,59 @@ int simpleHash(string input)
 	{
 		tool = tool + input[i];
 	}
-	hash = tool % 10;
+	hash = tool % input.size();
+	return hash;
 }
 
-//bloom filter function
+//bloom filter structure
 //Joseph DePoyster
 //May 3rd, 2018
 //implementation of a simple bloom filter that uses only two different hash functions
+//hash functions used convert strings to integers
+//both hash function's hashes are valued less than 200 (highest hash I got from slashHash was 117)
+struct bloomFilter
+{
+public:
+	bloomFilter();
+	void add(string s);
+	bool ask(string s);
+private:
+	bool bits[200];
+};
+
+bloomFilter::bloomFilter()
+{
+	for (int i = 0; i < 200; i++)
+	{
+		bits[i] = false;
+	}
+}
+
+void bloomFilter::add(string s) //sets position of hashes in the array to true. 
+{
+	int hash1 = slashHash(s);
+	int hash2 = simpleHash(s);
+
+	if (bits[hash1] == false)
+	{
+		bits[hash1] = true; //stores true for the location of the hash if it is false. 
+		//if the location is already true, than it will stay that way. 
+	}
+	if (bits[hash2] == false)
+	{
+		bits[hash2] = true; //ditto
+	}
+}
+
+bool bloomFilter::ask(string s)//checks to see if location of 
+{
+	int hash1 = slashHash(s);
+	int hash2 = simpleHash(s);
+	bool condition = false;
+
+	if (bits[hash1] == true && bits[hash2] == true)
+	{
+		condition = true;//returning true means there is a possibility that the input string is in the boom filter
+	}					 //returning false means the string definitly does not belong to the set.
+	return condition;
+}
